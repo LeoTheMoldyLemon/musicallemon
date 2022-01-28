8f40d6597b223c279f9208c53fccf9de57fd7b2c
LeoTheMoldyLemon <62692318+LeoTheMoldyLemon@users.noreply.github.com>
2022-01-28 14:45:44 +0100

    asd

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.on('error', error => {
 
 
 client.on("messageCreate", async (msg)=>{
-    console.log(msg)
+    console.log(msg.channel)
     console.log("Message received from, msg: "+msg.content)
     console.log("Server: "+msg.guild.name)
     if (msg.content[0]=="$"){
