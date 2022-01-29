ae90136e475defea21b2d6c173210ec8f1e14c8a
LeoTheMoldyLemon <62692318+LeoTheMoldyLemon@users.noreply.github.com>
2022-01-29 22:35:08 +0100

    remove broke, msg.reply thing to tell it doesnt work

--- index.js
+++ index.js
@@ -237,7 +237,8 @@ client.on("messageCreate", async (msg)=>{
                 }
             break;
             case "remove":
-                try{
+			msg.reply("This doesnt work rn, ill get around to fixing it... one day.")
+               /* try{
                     if(msg.guild.player.songqueue.length==1){
                         msg.guild.player.playerstate=false
                         msg.guild.player.stop()
@@ -259,7 +260,7 @@ client.on("messageCreate", async (msg)=>{
                 }catch(e){
                     msg.reply("I don't feel like it.")
                     console.log(e)
-                }
+                }*/
             break;
             case "halp":
                 msg.reply("This bot plays youtube music (or any video) in voice channels.\nCommands: \n`say` Replies to your message with what you told it to say.\n`play` Joins your voice channel and play the selected song or add it at the end of the queue. This command supports links, search querys and playlists.\n`pause` Pauses the current song.\n`continue` Unpauses.\n`queue` Displays songs in the queue.\n`clear` Clears queue and stops playing.\n`fuckoff` Fucks off.\n`goto` Plays the number of the song you entered.\n`move` Moves a song to a selected position in the queue.\n`skip` Skips to the next song in the queue.\n`loop` Toggles looping the queue.\n`remove` Removes the selected song.")
