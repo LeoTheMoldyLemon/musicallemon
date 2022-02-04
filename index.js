2c288b5ca3b0757b088fa675281cda0480a67c74
LeoTheMoldyLemon <62692318+LeoTheMoldyLemon@users.noreply.github.com>
2022-02-04 08:37:53 +0100

    download debug

--- index.js
+++ index.js
@@ -319,9 +319,9 @@ async function parseplaylist(arg, msg){
 function streamToString (stream) {
   const chunks = [];
   return new Promise((resolve, reject) => {
-    stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+    stream.on('data', (chunk) => console.log("receiving chunk"); chunks.push(Buffer.from(chunk)));
     stream.on('error', (err) => reject(err));
-    stream.on('end', () => resolve(Buffer.concat(chunks)));
+    stream.on('end', () => console.log("resovled"); resolve(Buffer.concat(chunks)));
   })
 }
 client.login(token);
